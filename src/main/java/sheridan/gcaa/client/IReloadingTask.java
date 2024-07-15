b84a59ac8248c876ac8be07d5ef8123b18c0152e
tjy1372@163.com <tjy1372@163.com>
2024-07-16 00:41:00 +0800

    修复reloadingTask导致服务端崩溃，下一步任务：带音效的动画读取

--- src/main/java/sheridan/gcaa/client/IReloadingTask.java
+++ src/main/java/sheridan/gcaa/client/IReloadingTask.java
@@ -2,13 +2,10 @@ package sheridan.gcaa.client;
 
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 /**
  * A logic controller that handles the reloading process on the client side and is typically instantiated and assigned to this ReloadingHandler INSTANCE when the reloading process is triggered, with the ReloadingHandler executing the task.
  * */
-@OnlyIn(Dist.CLIENT)
 public interface IReloadingTask {
     /**
      * return ture means this task is successfully completed.
