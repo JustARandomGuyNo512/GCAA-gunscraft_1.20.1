92114b6e068851a9dbf813bcb0860a01ed661499
tjy1372@163.com <tjy1372@163.com>
2024-08-25 23:22:09 +0800

    调整Akm左手模型姿势

---
+++ src/main/java/sheridan/gcaa/client/HandActionHandler.java
@@ -0,0 +1,11 @@
+package sheridan.gcaa.client;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class HandActionHandler {
+    public ItemStack itemStack;
+
+}
---
+++ src/main/java/sheridan/gcaa/client/IHandActionTask.java
@@ -0,0 +1,9 @@
+package sheridan.gcaa.client;
+
+import net.minecraft.world.entity.player.Player;
+
+public interface IHandActionTask {
+
+    void tick(Player clientPlayer);
+
+}
