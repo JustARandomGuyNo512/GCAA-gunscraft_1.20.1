11dc090699736afc09fab14a8bbd57b8f887479a
tjy1372@163.com <tjy1372@163.com>
2024-12-10 18:23:12 +0800

    修复换弹需要扣除多个弹药物品子弹时弹药计数错误

--- src/main/java/sheridan/gcaa/client/render/fx/muzzleSmoke/CommonMuzzleSmokeEffects.java
+++ src/main/java/sheridan/gcaa/client/render/fx/muzzleSmoke/CommonMuzzleSmokeEffects.java
@@ -8,6 +8,6 @@ import sheridan.gcaa.GCAA;
 
 @OnlyIn(Dist.CLIENT)
 public class CommonMuzzleSmokeEffects {
-    public static final MuzzleSmoke COMMON = new MuzzleSmoke(150, 3.5f, 3f, new Vector2f(0.9f, 0.7f),
+    public static final MuzzleSmoke COMMON = new MuzzleSmoke(150, 3.5f, 3f, new Vector2f(0.9f, 0.6f),
             new ResourceLocation(GCAA.MODID, "textures/fx/muzzle_smoke/common_0.png"), 4).randomRotate();
 }
