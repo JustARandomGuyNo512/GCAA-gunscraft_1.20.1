e5084a6868e0b333d427cec955043ee5eda949c7
tjy1372@163.com <tjy1372@163.com>
2024-12-13 01:30:44 +0800

    成功生成datapack

--- src/main/java/sheridan/gcaa/data/gun/GunPropertiesHandler.java
+++ src/main/java/sheridan/gcaa/data/gun/GunPropertiesHandler.java
@@ -1,8 +1,36 @@
 package sheridan.gcaa.data.gun;
 
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.NotNull;
 import sheridan.gcaa.GCAA;
+import sheridan.gcaa.items.gun.GunProperties;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Mod.EventBusSubscriber(modid = GCAA.MODID)
-public class GunPropertiesHandler {
+public class GunPropertiesHandler extends SimplePreparableReloadListener<Map<String, GunProperties>> {
+    public static GunPropertiesHandler INSTANCE;
+
+    @Override
+    protected @NotNull Map<String, GunProperties> prepare(@NotNull ResourceManager pResourceManager, @NotNull ProfilerFiller pProfiler) {
+        return new HashMap<>();
+    }
+
+    @SubscribeEvent
+    public static void addReloadListenerEvent(AddReloadListenerEvent event) {
+        GunPropertiesHandler handler = new GunPropertiesHandler();
+        event.addListener(handler);
+        INSTANCE = handler;
+    }
+
+    @Override
+    protected void apply(@NotNull Map<String, GunProperties> pObject, @NotNull ResourceManager pResourceManager, @NotNull ProfilerFiller pProfiler) {
+
+    }
 }
--- src/main/java/sheridan/gcaa/data/gun/GunPropertiesProvider.java
+++ src/main/java/sheridan/gcaa/data/gun/GunPropertiesProvider.java
@@ -1,4 +1,44 @@
 package sheridan.gcaa.data.gun;
 
-public class GunPropertiesProvider {
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.CachedOutput;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+import sheridan.gcaa.GCAA;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class GunPropertiesProvider implements DataProvider {
+    private final CompletableFuture<HolderLookup.Provider> registries;
+    protected final PackOutput.PathProvider pathProvider;
+    public GunPropertiesProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        this.pathProvider = output.createPathProvider(PackOutput.Target.DATA_PACK, "guns_properties");
+        this.registries = registries;
+    }
+
+    @Override
+    public @NotNull CompletableFuture<?> run(@NotNull CachedOutput pOutput) {
+        List<CompletableFuture<?>> list = new ArrayList<>();
+        String str = "abcdefghijklmn";
+        for (int i = 0 ; i < 10; i++) {
+            ResourceLocation key = new ResourceLocation(GCAA.MODID, "test_" + str.charAt(i));
+            Path path = this.pathProvider.json(key);
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("test", i);
+            list.add(DataProvider.saveStable(pOutput, jsonObject, path));
+        }
+        return CompletableFuture.allOf(list.toArray(CompletableFuture[]::new));
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return GCAA.MODID + ": guns_properties";
+    }
 }
