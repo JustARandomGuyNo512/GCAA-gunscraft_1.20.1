818c7642e79ab50e07faf2b088e816efb69fec5c
zhenghui-su <343196323@qq.com>
2025-01-23 21:24:52 +0800

    feat: 彩蛋文字增加

--- src/main/java/sheridan/gcaa/items/gun/guns/Annihilator.java
+++ src/main/java/sheridan/gcaa/items/gun/guns/Annihilator.java
@@ -1,5 +1,10 @@
 package sheridan.gcaa.items.gun.guns;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import sheridan.gcaa.items.ModItems;
 import sheridan.gcaa.items.gun.Gun;
 import sheridan.gcaa.items.gun.GunProperties;
@@ -19,5 +24,11 @@ public class Annihilator extends Gun {
                 3.4f, GunProperties.toRPM(800), getTicks(2.55f), getTicks(3.3f), 20,
                 1.6f, 0.5f, 0.15f, 0.12f, 12, List.of(Auto.AUTO),
                 ModSounds.ANNIHILATOR_FIRE, null, caliber));
+    }
+
+    @Override
+    protected void gunDetailInfo(ItemStack stack, @Nullable Level levelIn, List<Component> tooltip, TooltipFlag flagIn) {
+        super.gunDetailInfo(stack, levelIn, tooltip, flagIn);
+        tooltip.add(Component.translatable("tooltip.gun_info.annihilator"));
     }
 }
