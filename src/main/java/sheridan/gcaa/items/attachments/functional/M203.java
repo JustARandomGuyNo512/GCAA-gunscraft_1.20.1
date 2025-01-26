94179231d7413ba108e4240927ae5d767c594b88
tjy1372@163.com <tjy1372@163.com>
2025-01-26 15:37:20 +0800

    调整动画

--- src/main/java/sheridan/gcaa/items/attachments/functional/M203.java
+++ src/main/java/sheridan/gcaa/items/attachments/functional/M203.java
@@ -9,6 +9,6 @@ public class M203 extends GrenadeLauncher{
     public M203() {
         super(ModItems.AMMO_M433.get(),
                 new InertialRecoilData(0, 0, 1f, 0.08f, 1.3f,  0.06f, 0.5f, 0.25f),
-                5, 3f, ModSounds.M203_FIRE, RenderAndMathUtils.secondsToTicks(3.2f), 4.1f, 0.8f, 4f, 6);
+                5, 3f, ModSounds.M203_FIRE, RenderAndMathUtils.secondsToTicks(3.2f), 4f, 0.8f, 4f, 6);
     }
 }
