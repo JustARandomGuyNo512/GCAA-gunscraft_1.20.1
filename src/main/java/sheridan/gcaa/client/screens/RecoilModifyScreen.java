d7b4a5dc2ec2e7e109f19913a48cf2581c70153f
tjy1372@163.com <tjy1372@163.com>
2025-03-15 23:40:57 +0800

    后坐力配置界面UI完成

--- src/main/java/sheridan/gcaa/client/screens/RecoilModifyScreen.java
+++ src/main/java/sheridan/gcaa/client/screens/RecoilModifyScreen.java
@@ -20,9 +20,8 @@ import sheridan.gcaa.client.animation.recoilAnimation.*;
 import sheridan.gcaa.items.gun.IGun;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.HashMap;import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.List;
 
 @OnlyIn(Dist.CLIENT)
 public class RecoilModifyScreen extends Screen {
@@ -30,8 +29,8 @@ public class RecoilModifyScreen extends Screen {
     public IGun gun;
     public Map<String, List<TrackBox>> trackMap = new HashMap<>();
     public Map<String, NewRecoilData.Track> trackPool = new HashMap<>();
-    public List<AbstractWidget> springEditions = new ArrayList<>();
-    public List<AbstractWidget> trackEditions = new ArrayList<>();
+    public Set<AbstractWidget> springEditions = new HashSet<>();
+    public Set<AbstractWidget> trackEditions = new HashSet<>();
     public int mx, my;
     public EditBox impulseScript;
     public Button selectedSpringBtn;
@@ -41,22 +40,31 @@ public class RecoilModifyScreen extends Screen {
     public Button massDampingSpring;
     public Button steadyStateSpring;
     public boolean hide = false;
+    public boolean trackEditionVisible = true;
+    public boolean springEditionVisible = true;
+    public List<Label> labels = new ArrayList<>();
 
     @Override
     public void render(@NotNull GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
         super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
-        renderBackground(pGuiGraphics);
-        Font font = Minecraft.getInstance().font;
-        pGuiGraphics.drawString(font, "X trans",8, 30, 0xff0000);
-        pGuiGraphics.drawString(font, "Y trans",8, 45, 0x00ff00);
-        pGuiGraphics.drawString(font, "Z trans",8, 60, 0x0000ff);
-        pGuiGraphics.drawString(font, "X rot",8, 75, 0xff0000);
-        pGuiGraphics.drawString(font, "Y rot",8, 90, 0x00ff00);
-        pGuiGraphics.drawString(font, "Z rot",8, 105, 0x0000ff);
+        if (!hide) {
+            String warn = "Any action that triggers the interface to reload will result in the loss of unsaved data";
+            Font font = Minecraft.getInstance().font;
+            pGuiGraphics.drawString(font, warn, (int) (this.width / 2 - font.width(warn) / 2.5f), 10, 0xff0000);
+        }
 
-        pGuiGraphics.drawString(font, mx + " " + my, mx + font.width("."), my - font.lineHeight, 0xffffff);
-        pGuiGraphics.hLine(0, this.width, my, FastColor.ABGR32.color(255,0,255,0));
-        pGuiGraphics.vLine(mx, 0, this.height, FastColor.ABGR32.color(255,0,0,255));
+        renderBackground(pGuiGraphics);
+        if (!hide) {
+            for (Label label : labels) {
+                label.render(pGuiGraphics);
+            }
+        }
+
+        if (!hide) {
+            pGuiGraphics.drawString(this.font, mx + " " + my, mx + this.font.width("."), my - this.font.lineHeight, 0xffffff);
+            pGuiGraphics.hLine(0, this.width, my, FastColor.ABGR32.color(255,0,255,0));
+            pGuiGraphics.vLine(mx, 0, this.height, FastColor.ABGR32.color(255,0,0,255));
+        }
     }
 
     @Override
@@ -70,6 +78,7 @@ public class RecoilModifyScreen extends Screen {
         initRecoilData(rowHelper);
         initSpringEditions(rowHelper);
         initTrackEditions(rowHelper);
+        initLabels();
         Button fire = Button.builder(Component.literal("Fire"), (b) -> {
             Clients.MAIN_HAND_STATUS.buttonDown.set(true);
             Clients.DO_SEND_FIRE_PACKET = false;
@@ -80,7 +89,16 @@ public class RecoilModifyScreen extends Screen {
             hide = !hide;
             RecoilModifyScreen.this.renderables.forEach(r -> {
                 if (r instanceof AbstractWidget abstractWidget) {
-                    abstractWidget.visible = !hide;
+                    if (trackEditions.contains(r) || springEditions.contains(r)) {
+                        if (hide) {
+                            abstractWidget.visible = false;
+                        } else {
+                            abstractWidget.visible = trackEditions.contains(r) ?
+                                    trackEditionVisible : springEditionVisible;
+                        }
+                    } else {
+                        abstractWidget.visible = !hide;
+                    }
                 }
             });
             b.visible = true;
@@ -230,7 +248,7 @@ public class RecoilModifyScreen extends Screen {
         springEditions.add(clampedSpringBtn);
         springEditions.add(massDampingSpring);
         springEditions.add(steadyStateSpring);
-        springEditionsVisible(false);
+        //springEditionsVisible(false);
     }
 
     private void initTrackEditions(GridLayout.RowHelper rowHelper) {
@@ -248,38 +266,78 @@ public class RecoilModifyScreen extends Screen {
         }).size(25, 12).pos(80, 125).tooltip(Tooltip.create(Component.literal("Apply to track data in memory"))).build();
 
         impulseScript = new EditBox(Minecraft.getInstance().font, 40, 145, 345, 15, Component.literal(""));
-        impulseScript.setTooltip(Tooltip.create(Component.literal("Input impulse script here, example: 'Back*(A2.Steady*0.5+0.5)+UpRot'")));
+        impulseScript.setTooltip(Tooltip.create(Component.literal(
+                "Input impulse script here, example:\n 'Back*(A2.Steady*0.5+0.5)+UpRot'\nHover mouse on 'Variables' to see all available variables")));
+        impulseScript.setMaxLength(81);
+        variables = Button.builder(Component.literal("Variables"), (b) -> {}).size(40, 12).pos(345, 125)
+                .tooltip(Tooltip.create(Component.literal("All named Variables:\n"))).build();
 
         rowHelper.addChild(apply);
         rowHelper.addChild(flag);
         rowHelper.addChild(impulseScript);
+        rowHelper.addChild(variables);
         trackEditions.add(flag);
         trackEditions.add(apply);
         trackEditions.add(impulseScript);
+        trackEditions.add(variables);
+        trackEditionsVisible(false);
     }
 
     private void springEditionsVisible(boolean visible) {
         for (AbstractWidget widget : springEditions) {
             widget.visible = visible;
         }
+        springEditionVisible = visible;
     }
 
     private void trackEditionsVisible(boolean visible) {
         for (AbstractWidget widget : trackEditions) {
             widget.visible = visible;
         }
+        trackEditionVisible = visible;
     }
 
     private class TrackBox extends EditBox {
 
         public TrackBox(Font pFont, int pX, int pY, int pWidth, int pHeight, Component pMessage) {
             super(pFont, pX, pY, pWidth, pHeight, pMessage);
+            this.setMaxLength(2);
         }
 
         @Override
         public void onClick(double pMouseX, double pMouseY) {
             super.onClick(pMouseX, pMouseY);
             onTrackBoxClick(this);
+        }
+    }
+
+    private void initLabels() {
+        labels.add(new Label("X trans", 8, 30, 0xff0000));
+        labels.add(new Label("Y trans", 8, 45, 0x00ff00));
+        labels.add(new Label("Z trans", 8, 60, 0x0000ff));
+        labels.add(new Label("X rot", 8, 75, 0xff0000));
+        labels.add(new Label("Y rot", 8, 90, 0x00ff00));
+        labels.add(new Label("Z rot", 8, 105, 0x0000ff));
+    }
+
+    private static class Label {
+        public String str;
+        public int color;
+        public int x, y;
+
+        public Label(String str, int x, int y, int color) {
+            this.str = str;
+            this.color = color;
+            this.x = x;
+            this.y = y;
+        }
+
+        public Label(String str, int x, int y) {
+            this(str, x, y,0xffffff);
+        }
+
+        public void render(GuiGraphics graphics) {
+            graphics.drawString(Minecraft.getInstance().font, str, x, y, color);
         }
     }
 }
