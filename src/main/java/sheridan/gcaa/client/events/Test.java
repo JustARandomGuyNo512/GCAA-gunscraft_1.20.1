c7db43c744ad4f7917cf15a3e13aff3ce43416cb
Lucifer <1487303100@qq.com>
2025-06-28 16:18:11 +0800

    完成准备工作

--- src/main/java/sheridan/gcaa/client/events/Test.java
+++ src/main/java/sheridan/gcaa/client/events/Test.java
@@ -1,24 +1,19 @@
 package sheridan.gcaa.client.events;
 
 
-import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterShadersEvent;
-import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import sheridan.gcaa.GCAA;
-import sheridan.gcaa.client.model.io.GltfLoader;
-import sheridan.gcaa.client.model.modelPart.BufferedModelBone;
 import sheridan.gcaa.client.model.modelPart.GCAAShaderInstance;
 
 import java.io.IOException;
@@ -37,27 +32,6 @@ public class Test {
 
     public static ShaderInstance getTestShader() {
         return testShader;
-    }
-
-
-    @SubscribeEvent
-    public static void onRender(RenderLevelStageEvent event) {
-        if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_ENTITIES) {
-            LocalPlayer player = Minecraft.getInstance().player;
-            if (player != null && player.getMainHandItem().getItem() == Items.APPLE) {
-                BufferedModelBone.__test_render__(Minecraft.getInstance().getEntityRenderDispatcher().getPackedLightCoords(player, event.getPartialTick()));
-            }
-        }
-    }
-
-    static boolean test = false;
-    @SubscribeEvent
-    public static void test(TickEvent.ClientTickEvent event) {
-        if (event.phase == TickEvent.Phase.END && !test) {
-            System.out.println("Player logged in test load gltf");
-            GltfLoader.test();
-            test = true;
-        }
     }
 
     public static ShaderInstance SHADER_FOR_ENTITY_CUTOUT;
